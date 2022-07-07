6dd83588c22495454c6ab57dc0d337137e91201a
Enzo Barrett <enzo.b@tendeg.com>
2022-07-07 11:04:14 -0600

    fix formatting

--- src/SystemErrorSimulationDataGenerator.h
+++ src/SystemErrorSimulationDataGenerator.h
@@ -9,27 +9,27 @@ class SystemErrorAnalyzerSettings;
 
 class SystemErrorSimulationDataGenerator
 {
-public:
-	SystemErrorSimulationDataGenerator();
-	~SystemErrorSimulationDataGenerator();
+    public:
+        SystemErrorSimulationDataGenerator();
+        ~SystemErrorSimulationDataGenerator();
 
-	void Initialize( U32 simulation_sample_rate, SystemErrorAnalyzerSettings* settings );
-	U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channel );
+        void Initialize( U32 simulation_sample_rate, SystemErrorAnalyzerSettings* settings );
+        U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channel );
 
-protected:
-	SystemErrorAnalyzerSettings* m_Settings;
-	U32 m_SimulationSampleRateHz;
+    protected:
+        SystemErrorAnalyzerSettings* m_Settings;
+        U32 m_SimulationSampleRateHz;
 
-protected:
-    // create the data for one error display
-	void CreatePeriod();
+    protected:
+        // create the data for one error display
+        void CreatePeriod();
 
-    // display a digit at hz
-    void PulseDigit(int digit, int freq);
+        // display a digit at hz
+        void PulseDigit( int digit, int freq );
 
-	SimulationChannelDescriptor m_SerialSimulationData;
-	ClockGenerator m_Clock;
+        SimulationChannelDescriptor m_SerialSimulationData;
+        ClockGenerator m_Clock;
 
-    int m_ErrorCode;
+        int m_ErrorCode;
 };
-#endif //SYSTEMERROR_SIMULATION_DATA_GENERATOR
+#endif // SYSTEMERROR_SIMULATION_DATA_GENERATOR
